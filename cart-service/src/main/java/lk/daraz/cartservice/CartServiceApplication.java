6dca4d2df72cdb9723c726188907cb600f272b45
Daraz Dev <dev@daraz.lk>
2026-07-20 15:59:40 +0530

    feat(cart-service): bootstrap module - pom.xml + CartServiceApplication

---
+++ cart-service/src/main/java/lk/daraz/cartservice/CartServiceApplication.java
@@ -0,0 +1,16 @@
+package lk.daraz.cartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class CartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartServiceApplication.class, args);
+    }
+}
