93848a939947692f82a95b6d92a0ef1df11981b9
Daraz Dev <dev@daraz.lk>
2026-07-20 16:25:00 +0530

    feat(wishlist-service): bootstrap module - pom.xml + WishlistServiceApplication

---
+++ wishlist-service/src/main/java/lk/daraz/wishlistservice/WishlistServiceApplication.java
@@ -0,0 +1,16 @@
+package lk.daraz.wishlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class WishlistServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistServiceApplication.class, args);
+    }
+}
